9ff85eda84990ec2b3c9befbee0fcccfd4355866
AndroidLoaders <codes1589@gmail.com>
2020-05-12 14:52:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/motionlayout/MotionScene1.kt
@@ -0,0 +1,12 @@
+package com.example.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionScene1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.motion_scene_1)
+    }
+}
