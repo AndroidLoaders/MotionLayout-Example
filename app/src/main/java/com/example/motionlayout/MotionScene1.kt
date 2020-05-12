57951a02b439b8e3257412517679ca4bf6a3b7bd
AndroidLoaders <codes1589@gmail.com>
2020-05-12 16:31:50 +0530

    Motion 1 implemented

--- app/src/main/java/com/example/motionlayout/MotionScene1.kt
+++ app/src/main/java/com/example/motionlayout/MotionScene1.kt
@@ -1,9 +1,14 @@
 package com.example.motionlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MotionScene1 : AppCompatActivity() {
+
+    /**
+     * Learn More about MotionLayout Here :
+     * https://developer.android.com/training/constraint-layout/motionlayout#androidx
+     * */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
